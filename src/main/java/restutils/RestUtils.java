0d4e4543ba7ad617cf4a33a0483227c7375a3f4f
akhil-bidgely <128053886+akhil-bidgely@users.noreply.github.com>
2023-04-21 16:46:01 +0530

    New changes in structure

--- src/main/java/restutils/RestUtils.java
+++ src/main/java/restutils/RestUtils.java
@@ -5,6 +5,8 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
+import java.util.Properties;
+
 public class RestUtils {
 
 
@@ -17,6 +19,20 @@ public class RestUtils {
         RequestSpecification given = RestAssured.given();
         Response response = given.baseUri(endpoint).basePath(basePath + uuid)
                 .header("Content-Type", "application/json").header("Authorization", "Bearer" + token)
+                .get();
+
+        return response;
+
+    }
+    public static Response generateToken(String endpoint)
+    {
+
+        String pathParam="/oauth/token?grant_type=client_credentials&scope=all";
+        RequestSpecification given = RestAssured.given();
+        Response response = given.baseUri(endpoint)
+                .basePath(pathParam)
+                .header("Content-Type", "application/json").auth().basic("akhil@bidgely.com","ab0ufM@bK630")
+                .params("grant_type","client_credentials").params("scope","all")
                 .get();
 
         return response;
